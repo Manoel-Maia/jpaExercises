642f5352ac801e46d1cdc875524d861086ff0126
Manoel Maia <manoelh776@gmail.com>
2026-06-09 19:08:48 -0300

    Revert "incrementar comentario aleatorio"
    This reverts commit b18655c7c1adac4901d3fcd7c5908e77c569d098.

--- jpaExercises/src/base/User.java
+++ jpaExercises/src/base/User.java
@@ -11,7 +11,7 @@ public class User {
 	private String name;
 	private String email;
 	
-	///sdfsdflsdkjfsdfs
+	///sdfsdflsdkjf
 	public User(String name, String email) {
 		this();;;
 		this.name = name;
